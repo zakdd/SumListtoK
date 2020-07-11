d642dfa543ff7b317f9d1d97355a7bb8c9a516e4
Zakaria <daud0009@d.umn.edu>
2020-07-10 21:19:36 -0500

    Finished in one pass

--- SumToK.py
+++ SumToK.py
@@ -1,4 +1,15 @@
 #Given a list of numbers and a number k, return whether any two numbers from the list add up to k
 
-NumberSet = [10, 15, 3, 7]
+
+sample_set = set()
+
+arr = [10, 15, 3, 7]
+
+sample_set.update(arr)
+
+k = 17 
+
+for i in arr:
+    if k-i in sample_set:
+        print(i)
 
